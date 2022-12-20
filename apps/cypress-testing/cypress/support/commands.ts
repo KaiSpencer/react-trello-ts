da4f3d081baca50834ca84894974d44740df4f6f
Kai <kaispencer98@gmai.com>
2022-12-20 22:55:17 +0000

    Test for dragging card in its own lane

--- apps/cypress-testing/cypress/support/commands.ts
+++ apps/cypress-testing/cypress/support/commands.ts
@@ -12,11 +12,31 @@
 //
 // -- This is a parent command --
 Cypress.Commands.add("findCardByTitle", (title: string) => {
-  return cy
-    .get(".horizontal")
-    .findByText(title)
-    .should("exist");
+	return cy.get(".horizontal").findByText(title).should("exist");
 });
+
+Cypress.Commands.add(
+	"findCardByTitleInLane",
+	(title: string, laneIdx: number) => {
+		return cy
+			.get(`.horizontal > :nth-child(${laneIdx + 1})`)
+			.findByText(title)
+			.should("exist");
+	},
+);
+
+Cypress.Commands.add(
+	"assertCardIndexInLane",
+	{ prevSubject: "element" },
+	(subject, idx: number) => {
+		cy.wrap(
+			subject.parents(".horizontal").children()[0].children[1].children[0]
+				.children[idx],
+		)
+			.findByText(subject.text())
+			.should("exist");
+	},
+);
 //
 //
 // -- This is a child command --
@@ -31,15 +51,20 @@ Cypress.Commands.add("findCardByTitle", (title: string) => {
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
 declare global {
-  namespace Cypress {
-    interface Chainable {
-      // login(email: string, password: string): Chainable<void>
-      // drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-      // dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-      // visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-      findCardByTitle(title: string): Chainable<JQuery<HTMLElement>>;
-    }
-  }
+	namespace Cypress {
+		interface Chainable {
+			// login(email: string, password: string): Chainable<void>
+			// drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+			// dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+			// visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+			findCardByTitle(title: string): Chainable<JQuery<HTMLElement>>;
+			findCardByTitleInLane(
+				title: string,
+				laneIdx: number,
+			): Chainable<JQuery<HTMLElement>>;
+			assertCardIndexInLane(idx: number): Chainable<JQuery<HTMLElement>>;
+		}
+	}
 }
 import "@testing-library/cypress/add-commands";
 import "@4tw/cypress-drag-drop";
