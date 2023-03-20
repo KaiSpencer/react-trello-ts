d90e907af08f9f20780526311e99ea4664576c97
KaiSpencer <kaispencer98@gmail.com>
2023-03-20 16:03:33 +0000

    Change cypress example to be react 17

--- apps/cypress-testing/vite.config.ts
+++ apps/cypress-testing/vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	resolve:{
+		alias: {
+			"@": "../../node_modules/react-trello-ts/src"
+		}
+	}
 });
